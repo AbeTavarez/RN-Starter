21268da4c6236346f999cf9afddb238f3d38868c
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-04-12 23:35:39 -0400

    navigation buttons added

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Home</Text>;
+  return (
+    <View>
+      <Text style={styles.text}>Home Screen</Text>
+
+      <Button 
+      title='Go to Components Demo'
+      onPress={() => console.log('btn pressed')}
+      />
+
+      <TouchableOpacity onPress={() => console.log('List Pressed')}>
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity>
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
+    paddingBottom: 20
   },
 });
 
